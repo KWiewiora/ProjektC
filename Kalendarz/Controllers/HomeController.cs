14d2fe161e9f3c696d9cc1e9ed5945156dc4fb41
KWiewiora <krzysztof0wiewiora@gmail.com>
2018-11-18 23:18:37 +0100

    stworzenie kalendarza polaczonego z baza danych

---
+++ Kalendarz/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kalendarz.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (BazaDanychEntities dc = new BazaDanychEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
